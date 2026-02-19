d20991478cf1ce15ac47979c3e5f04c1fb586ba8
Walter van der Giessen <waltervdgiessen@gmail.com>
2026-02-19 14:00:40 +0100

    Add ts-nocheck comment for auto-generated worker files

--- src/lib/lib.ts
+++ src/lib/lib.ts
@@ -141,8 +141,14 @@ globalThis.__worker_wrapper__ = async (
       }
 
       const wrapper = `(({${finalVars.join(",")}}) => ${fnStr})`;
-      const finalWorkerCode = patchedCode +
+      let finalWorkerCode = patchedCode +
         WORKER_BODY.replace("%FN%", wrapper);
+
+      const nocheckPrefix = "// @ts-nocheck: Auto-generated worker file\n";
+
+      if (!finalWorkerCode.startsWith(nocheckPrefix)) {
+        finalWorkerCode = nocheckPrefix + finalWorkerCode;
+      }
 
       const hash = createHash("md5").update(signatureKey).digest("hex");
       const workerDir = resolve(process.cwd(), ".workers");
